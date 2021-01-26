8991653504c98ddc7d1a24f1ff7354067d2515ee
mabel <bigbelk@live.ru>
2021-01-26 19:23:58 +0300

    akhmatova

--- frontend/src/components/Person.svelte
+++ frontend/src/components/Person.svelte
@@ -1,9 +1,36 @@
 <script>
     import {onMount} from 'svelte'
+    //import * as vis from 'vis-netwonk'
+
+    let canvasHolder 
+
+    let nodes = new vis.DataSet([
+        { id: 1, label: "А. Ахматова" },
+        { id: 2, label: "Л. Гумилев" },
+        { id: 3, label: "Н. Гумилев" },
+    ])
+
+    let edges = new vis.DataSet([
+        { from: 1, to: 3 },
+        { from: 1, to: 2 },
+    ])
+
+    onMount(()=>{
+      let data = {
+        nodes: nodes,
+        edges: edges,
+      }
+      //var DOTstring = 'dinetwork {1 -> 1 -> 2; 2 -> 3; 2 -- 4; 2 -> 1 }';
+      //var DOTstring = ' dinetwork { a [label="А. Ахматова"]; n [label="Н. Гумилев"]; l [label="Л. Гумилев"]; a -> n [label="муж"]; a -> l [label="сын"]; }'
+      //data = vis.parseDOTNetwork(DOTstring);
+      let network = new vis.Network(canvasHolder, {nodes, edges}, {});
+        
+    })
+
+
 </script>
 
 <figure>
-    <canvas>
-    </canvas>
+    <div class="canvas-holder" bind:this={canvasHolder}></div>
     <figcaption>Get browser details using.</figcaption>
 </figure>

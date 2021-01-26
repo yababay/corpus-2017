a8972d3422df51aabaa57be4e7bc6f1edf50295f
mabel <bigbelk@live.ru>
2021-01-27 00:31:37 +0300

    logo

--- frontend/src/components/Person.svelte
+++ frontend/src/components/Person.svelte
@@ -1,30 +1,13 @@
 <script>
     import {onMount} from 'svelte'
-    //import * as vis from 'vis-netwonk'
+    import {parseDOTNetwork, Network} from './vis-network.min.js'
 
     let canvasHolder 
 
-    let nodes = new vis.DataSet([
-        { id: 1, label: "А. Ахматова" },
-        { id: 2, label: "Л. Гумилев" },
-        { id: 3, label: "Н. Гумилев" },
-    ])
-
-    let edges = new vis.DataSet([
-        { from: 1, to: 3 },
-        { from: 1, to: 2 },
-    ])
-
-    onMount(()=>{
-      let data = {
-        nodes: nodes,
-        edges: edges,
-      }
-      //var DOTstring = 'dinetwork {1 -> 1 -> 2; 2 -> 3; 2 -- 4; 2 -> 1 }';
-      //var DOTstring = ' dinetwork { a [label="А. Ахматова"]; n [label="Н. Гумилев"]; l [label="Л. Гумилев"]; a -> n [label="муж"]; a -> l [label="сын"]; }'
-      //data = vis.parseDOTNetwork(DOTstring);
-      let network = new vis.Network(canvasHolder, {nodes, edges}, {});
-        
+    onMount(() => {
+        fetch('data/akhmatova.dot')
+            .then(res => res.text())
+            .then(data => new Network(canvasHolder, parseDOTNetwork(data)))
     })
 
 

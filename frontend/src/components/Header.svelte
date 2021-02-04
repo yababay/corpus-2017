7f2032867f3c04df00c3674c02cd0e8ee172126e
mabel <bigbelk@live.ru>
2021-02-04 23:46:47 +0300

    come-readme

--- frontend/src/components/Header.svelte
+++ frontend/src/components/Header.svelte
@@ -1,7 +1,7 @@
 <header>
     <img src="img/logo.svg">
     <ul>
-        <li><a href="#/about">О проекте</a></li>
+        <li><a href="#/about">О проекте</a></li>   
         <li><a href="#/premium">Перспективы</a></li>
         <li><a href="#/contacts">Контакты</a></li>
     </ul>

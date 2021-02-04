d6b76ffab27607dbc8469ee333e5b9e711695b79
mabel <bigbelk@live.ru>
2021-02-04 19:12:07 +0300

    patreon

--- frontend/src/components/Header.svelte
+++ frontend/src/components/Header.svelte
@@ -2,7 +2,7 @@
     <img src="img/logo.svg">
     <ul>
         <li><a href="#/about">О проекте</a></li>
-        <li><a href="#/premium">Полная версия</a></li>
+        <li><a href="#/premium">Перспективы</a></li>
         <li><a href="#/contacts">Контакты</a></li>
     </ul>
 </header>
